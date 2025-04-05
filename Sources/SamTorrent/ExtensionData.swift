31774d2c47fd61ff255c800c5400f1ae6202b566
Sam <30577766+Samasaur1@users.noreply.github.com>
2025-04-05 14:21:33 -0700

    fix extension protocol bit

--- Sources/SamTorrent/ExtensionData.swift
+++ Sources/SamTorrent/ExtensionData.swift
@@ -3,10 +3,11 @@ import Foundation
 struct ExtensionData: OptionSet {
     let rawValue: UInt64
 
-    // static let dht: ExtensionData = ExtensionData(rawValue: 1 << 0) // BEP0005
+    // I'd like to use bit shifting, but I was running into weird errors
+    // that I think were due to endianness
     static let dht: ExtensionData = ExtensionData(from: Data([0,0,0,0,0,0,0,0x01])) // BEP0005
     static let fast: ExtensionData = ExtensionData(from: Data([0,0,0,0,0,0,0,0x04])) // BEP0006
-    static let `extension`: ExtensionData = ExtensionData(from: Data([0,0,0,0,0x10,0,0,0])) //BEP0010
+    static let `extension`: ExtensionData = ExtensionData(from: Data([0,0,0,0,0,0x10,0,0])) //BEP0010
 
     static let supportedByMe: ExtensionData = []
 }
